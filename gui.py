ca605d293073e61f45cd546aa3a13d5d22229284
eattrenclenhard <125723190+eattrenclenhard@users.noreply.github.com>
2024-12-19 15:11:07 +0800

    Change theme, add clock, handle exit and complete errors

--- gui.py
+++ gui.py
@@ -1,9 +1,20 @@
+import time
 import modules.functions as ft
 import PySimpleGUI as sg
+import os
 
+if not os.path.exists('todos.txt'):
+    with open('todos.txt', 'w') as f:
+        pass
+
+sg.theme("LightGreen5")
+
+clock = sg.Text('', key='clock')
 label = sg.Text('Type in a To-Do')
 input_box = sg.InputText(tooltip='Enter todo', key='todo_input')  # no argument required
-add_button = sg.Button("Add")
+# add_button = sg.Button(size=2, image_source='images/add.png', mouseover_colors='Green', tooltip='Add Todo',
+#                        key='Add')
+add_button = sg.Button('Add')  # event takes either button_text label as value
 list_box = sg.Listbox(values=ft.get_todos(), key='todo_item',
                       enable_events=True, size=(45, 10))  # notice the logging of event at each item click
 edit_button = sg.Button('Edit')
@@ -12,6 +23,7 @@ exit_button = sg.Button('Exit')
 
 window = sg.Window('My To-Do App',
                    layout=[
+                       [clock],
                        [label], [input_box, add_button],
                        [list_box, edit_button, complete_button],
                        [exit_button]
@@ -23,10 +35,8 @@ window = sg.Window('My To-Do App',
 # each nested list in the layout argument represents a row
 
 while True:
-    event, values = window.read()  # will stay at this line so long as the window stays open
-    print('event:', event)
-    print('values:', values)
-    print("values['todo_item']:", values['todo_item'])
+    event, values = window.read(timeout=200)  # will stay at this line so long the window stays open
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case 'Add':
             todos = ft.get_todos()
@@ -35,20 +45,26 @@ while True:
             ft.write_todos(todos)
             window['todo_item'].update(values=todos)
         case 'Edit':
-            todo_to_edit = values['todo_item'][0]
-            new_todo = values['todo_input']
-            todos = ft.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo + '\n'
-            ft.write_todos(todos_param=todos)
-            # refreshes the list in-place
-            window['todo_item'].update(values=todos)
+            try:
+                todo_to_edit = values['todo_item'][0]
+                new_todo = values['todo_input']
+                todos = ft.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo + '\n'
+                ft.write_todos(todos_param=todos)
+                # refreshes the list in-place
+                window['todo_item'].update(values=todos)
+            except IndexError as e:
+                sg.popup('Please select an item first.', font=('Helvetica', 20))
         case 'Complete':
-            todos = ft.get_todos()
-            todos.remove(values['todo_item'][0])
-            ft.write_todos(todos_param=todos)
-            window['todo_item'].update(values=todos)
-            window['todo_input'].update(value='')
+            try:
+                todos = ft.get_todos()
+                todos.remove(values['todo_item'][0])
+                ft.write_todos(todos_param=todos)
+                window['todo_item'].update(values=todos)
+                window['todo_input'].update(value='')
+            except IndexError as e:
+                sg.popup('Please select an item first.', font=('Helvetica', 20))
         case 'Exit':
             print('Tq for choosing us! Shutting down...')
             break
@@ -62,5 +78,4 @@ while True:
             # this is implemented so that input box becomes dynamic in nature. It would now display whichever todo item the user selects
             window['todo_input'].update(value=values['todo_item'][0])
 
-print('sparta!')
 window.close()
